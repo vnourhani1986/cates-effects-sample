ce0a572f94e4cb867e944682a2e836abbb0c0d10
vahid nourhani <vnourhani1986@gmail.com>
2020-11-23 14:53:09 +0330

    add meta file

--- build.sbt
+++ build.sbt
@@ -4,8 +4,15 @@ version := "2.2.0"
 
 scalaVersion := "2.12.8"
 
+val circeVersion = "0.12.3"
+
 libraryDependencies += "org.typelevel" %% "cats-effect" % "2.2.0" withSources() withJavadoc()
 libraryDependencies += "com.codecommit" %% "cats-effect-testing-scalatest" % "0.4.2" % Test
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
 
 scalacOptions ++= Seq(
   "-feature",
--- src/main/scala/CopyFile.scala
+++ src/main/scala/CopyFile.scala
@@ -2,22 +2,63 @@ import java.io._
 import cats.effect._
 import cats.syntax.all._
 import scala.util.Try
-
+import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
 
 object CopyFile {
-  def apply(origin: File, destination: File, bufferSize: Int): IO[Long] =
-    inputOutputStreams(origin, destination).use { case (in, out) =>
-      transfer(in, out, bufferSize)
+  def apply(origin: File,
+            destination: File,
+            meta: File,
+            bufferSize: Int): IO[Long] =
+    for {
+      size <- inputOutputStreams(origin, destination).use {
+        case (in, out) =>
+          transfer(in, out, bufferSize)
+      }
+      _ <- createMeta(meta, origin.getPath, destination.getPath, size)
+    } yield size
+
+  def writeMeta(outputStream: OutputStream, meta: Array[Byte]): IO[Unit] =
+    IO(outputStream.write(meta, 0, meta.length))
+
+  def createMeta(file: File,
+                 origin: String,
+                 destination: String,
+                 size: Long): IO[Unit] =
+    outputStream(file).use { out =>
+      for {
+        calc <- calculateUnit(size)
+        (scaledSize, unit) = calc
+        buffer <- IO(
+          Meta(origin, destination, Volume(scaledSize, unit)).asJson.noSpaces.getBytes)
+        result <- writeMeta(out, buffer)
+      } yield result
     }
 
-  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] =
+  def calculateUnit(size: Long): IO[(Long, String)] =
+    IO {
+      if (size < Unit.Size.Kilo) (size, Unit.Symbol.Byte)
+      else if (size < Unit.Size.Mega) (size / Unit.Size.Kilo, Unit.Symbol.Kilo)
+      else if (size < Unit.Size.Giga) (size / Unit.Size.Mega, Unit.Symbol.Mega)
+      else (size / Unit.Size.Giga, Unit.Symbol.Giga)
+    }
+
+  def transmit(origin: InputStream,
+               destination: OutputStream,
+               buffer: Array[Byte],
+               acc: Long): IO[Long] =
     for {
       amount <- IO(origin.read(buffer, 0, buffer.size))
-      count <- if (amount > -1) IO(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
+      count <- if (amount > -1)
+        IO(destination.write(buffer, 0, amount)) >> transmit(origin,
+                                                             destination,
+                                                             buffer,
+                                                             acc + amount)
       else IO.pure(acc)
     } yield count
 
-  def transfer(origin: InputStream, destination: OutputStream, bufferSize: Int): IO[Long] =
+  def transfer(origin: InputStream,
+               destination: OutputStream,
+               bufferSize: Int): IO[Long] =
     for {
       buffer <- IO(new Array[Byte](bufferSize))
       total <- transmit(origin, destination, buffer, 0)
@@ -34,10 +75,12 @@ object CopyFile {
     Resource.make {
       IO(new FileOutputStream(f))
     } { outStream =>
-      IO(outStream.close).handleErrorWith(_ => IO(println("output file closed")))
+      IO(outStream.close).handleErrorWith(_ =>
+        IO(println("output file closed")))
     }
 
-  def inputOutputStreams(in: File, out: File): Resource[IO, (InputStream, OutputStream)] =
+  def inputOutputStreams(in: File,
+                         out: File): Resource[IO, (InputStream, OutputStream)] =
     for {
       inStream <- inputStream(in)
       outStream <- outputStream(out)
@@ -45,30 +88,59 @@ object CopyFile {
 
 }
 
+case class Meta(
+    input: String,
+    output: String,
+    volume: Volume
+)
+
+case class Volume(
+    size: Long,
+    unit: String
+)
+
+object Unit {
+  object Size {
+    val Byte = 1L
+    val Kilo = 1000L
+    val Mega = 1000000L
+    val Giga = 1000000000L
+  }
+
+  object Symbol {
+    val Byte = "B"
+    val Kilo = "K"
+    val Mega = "M"
+    val Giga = "G"
+  }
+
+}
 
 object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] =
     for {
-      _ <- if (args.length < 3) IO.raiseError(new IllegalArgumentException(" need origin and destination files"))
+      _ <- if (args.length < 3)
+        IO.raiseError(
+          new IllegalArgumentException(" need origin and destination files"))
       else IO.unit
       bufferSize <- Try {
         args(2).toInt
       }.toEither match {
-          case Right(value) => IO.pure(value)
-          case Left(ex) => IO.raiseError(ex)
+        case Right(value) => IO.pure(value)
+        case Left(ex)     => IO.raiseError(ex)
       }
-      _ <- if (args(0) == args(1)) IO.raiseError(new IllegalArgumentException("origin and destination are same"))
+      _ <- if (args(0) == args(1))
+        IO.raiseError(
+          new IllegalArgumentException("origin and destination are same"))
       else IO.unit
       orig <- IO(new File(args(0)))
       dest <- IO(new File(args(1)))
-      //      _ <- if (dest.exists()) Resource.make {
-      //        IO( new Scanner(System.in))
-      //      } { inScanner =>
-      //        IO(inScanner).withErrorHandle(_ => IO(println("input scanner closed")))
-      //      }
-      //      else IO.unit
-      count <- CopyFile(orig, dest, bufferSize)
-      _ <- IO(println(s"$count bytes copied from ${orig.getPath} to ${dest.getPath}"))
+      meta <- IO(
+        new File(args(1).substring(0, args(1).indexOf('.')) + ".meta.json"))
+      count <- CopyFile(orig, dest, meta, bufferSize)
+      _ <- IO(
+        println(s"$count bytes copied from ${orig.getPath} to ${dest.getPath}"))
     } yield ExitCode.Success
+
 }
